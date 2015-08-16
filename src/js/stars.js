a7f761f7437ddea3d9876149051b9a8984f1f45d
lucas <lucaspenney@gmail.com>
2015-08-15 20:28:03 -0700

    Implement flickering stars effect

--- src/js/stars.js
+++ src/js/stars.js
@@ -15,19 +15,24 @@ function Stars(screen) {
 Stars.prototype.init = function(screen) {
     this.layers = [{
         stars: [],
-        amount: 500,
-        scale: 0.2,
+        amount: 50,
+        scale: 0.8,
         size: 0.1,
     }, {
         stars: [],
-        amount: 200,
-        scale: 0.5,
-        size: 1,
+        amount: 100,
+        scale: 0.6,
+        size: 0.5,
     }, {
         stars: [],
-        amount: 50,
-        scale: 0.8,
-        size: 2.5,
+        amount: 100,
+        scale: 0.4,
+        size: 1.3,
+    }, {
+        stars: [],
+        amount: 100,
+        scale: 0.2,
+        size: 1.9,
     }, ];
     for (var l = 0; l < this.layers.length; l++) {
         var layer = this.layers[l];
@@ -37,7 +42,9 @@ Stars.prototype.init = function(screen) {
             layer.stars.push({
                 x: x,
                 y: y,
-                size: Math.floor(Math.random() * 2) + layer.size
+                size: Math.floor(Math.random() * 3) + layer.size,
+                flicker: 0,
+                seed: Math.random() + 0.3
             });
         }
     }
@@ -67,10 +74,38 @@ Stars.prototype.render = function(ctx, screen) {
                 //layer.stars[i].y = yOffset - (Math.random() * 100);
                 layer.stars[i].y -= screen.height;
             }
+
             ctx.fillStyle = "#FFF";
-            ctx.fillRect(layer.stars[i].x - xOffset, layer.stars[i].y - yOffset, layer.stars[i].size, layer.stars[i].size);
+            //ctx.fillRect(layer.stars[i].x - xOffset, layer.stars[i].y - yOffset, layer.stars[i].size, layer.stars[i].size);
+            layer.stars[i].flicker += (Math.random() - 0.5) * 0.5;
+            if (Math.abs(layer.stars[i].flicker) > 0.3) layer.stars[i].flicker = 0;
+            this.drawStar(ctx, layer.stars[i].x - xOffset, layer.stars[i].y - yOffset, Math.floor(2 + layer.stars[i].size * 2), layer.stars[i].size + Math.random() + layer.stars[i].flicker, layer.stars[i].seed + layer.stars[i].flicker);
         }
     }
-}
+};
+
+Stars.prototype.drawStar = function(ctx, cx, cy, spikes, outerRadius, innerRadius) {
+    var rot = Math.PI / 2 * 3;
+    var x = cx;
+    var y = cy;
+    var step = Math.PI / spikes;
+
+    ctx.beginPath();
+    ctx.moveTo(cx, cy - outerRadius)
+    for (i = 0; i < spikes; i++) {
+        x = cx + Math.cos(rot) * outerRadius;
+        y = cy + Math.sin(rot) * outerRadius;
+        ctx.lineTo(x, y)
+        rot += step
+
+        x = cx + Math.cos(rot) * innerRadius;
+        y = cy + Math.sin(rot) * innerRadius;
+        ctx.lineTo(x, y)
+        rot += step
+    }
+    ctx.lineTo(cx, cy - outerRadius)
+    ctx.fill();
+    ctx.closePath();
+};
 
 module.exports = Stars;
